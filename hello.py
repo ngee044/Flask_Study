009e78a8a9feacd6c5b8b558c026ccc0af64c2ef
LeeHyunKyu <mgee044@naver.com>
2021-10-14 01:54:25 +0900

    Flask Methods Tutorial
    Python Code solution 1, 2
    
    solution 1)
    if 분기에 따른 공통 로직
    
    solution 2)
    공통 로직을 타지 않아도 될 때 사용

--- hello.py
+++ hello.py
@@ -2,14 +2,25 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/users/<username>')
-def get_user(username):
-    return username
+#solution 1
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
 
-@app.route('/posts/<int:post_id>')
-def get_post(post_id):
-    return str(post_id)
+#solution 2
+@app.route('/login', methods=['GET'])
+def login_page():
+    return show_the_login_form()
 
-@app.route('/uuid/<uuid:uuid>')
-def get_uuid(uuid):
-    return str(uuid)
+@app.route('/login', methods=['POST'])
+def login():
+    return do_the_login()
+
+def show_the_login_form():
+    pass
+
+def do_the_login():
+    pass
