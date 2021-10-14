fe1faef6e000f1967357fa17dc04a99c3adb09e7
LeeHyunKyu <mgee044@naver.com>
2021-10-14 23:31:28 +0900

    static file & templates
    Flask에서 정적 파일과 템플릿 사용법
    static folder와 template folder 파일을 찾는 방벙에 대한 스터디 커밋
    import render_template을 이요한 방법

--- hello.py
+++ hello.py
@@ -1,26 +1,7 @@
-from flask import Flask, request
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
-#solution 1
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return do_the_login()
-    else:
-        return show_the_login_form()
-
-#solution 2
-@app.route('/login', methods=['GET'])
-def login_page():
-    return show_the_login_form()
-
-@app.route('/login', methods=['POST'])
-def login():
-    return do_the_login()
-
-def show_the_login_form():
-    pass
-
-def do_the_login():
-    pass
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
