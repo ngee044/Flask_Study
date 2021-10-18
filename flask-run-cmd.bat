22d2c478973c15a459fb349977e109bebc7c4039
LeeHyunKyu <mgee044@naver.com>
2021-10-19 02:17:33 +0900

    템플릿 엔진 사용(jinja2)
    templateEngin Jinja2 (Flask Lib)
    
    Add flask-run-cmd.bat

---
+++ flask-run-cmd.bat
@@ -0,0 +1,2 @@
+set FLASK_APP=hello.py
+set FLASK_ENV=development
--- hello.py
+++ hello.py
@@ -3,5 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/hello')
-def hello():
-    return render_template('hello.html')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
