bc52540b04259993f8d028ad9a8eb10db100b530
LeeHyunKyu <mgee044@naver.com>
2021-10-20 02:14:14 +0900

    redirect function tutorial
    리다이렉션과 에러

--- hello.py
+++ hello.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, abort
 
-app = Flask(__name__, static_folder='static', template_folder='templates')
+app = Flask(__name__)
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.errorhandler(403)
+def permission_denied(error):
+    return '403', 403
+
+@app.route('/')
+def index():
+    return redirect(url_for('user_list'))
+
+@app.route('/users')
+def user_list():
+    abort(403)
+
+
